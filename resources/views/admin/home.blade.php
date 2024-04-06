fe9da8b5a02308c932e7668530d35272686ae200
Sunny|Thien_An <thienantang123@gmail.com>
2024-04-06 23:53:50 +0700

    code mới nè

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -19,7 +19,7 @@
                     </div>
                     <div class="admin-content-main-content">
                         <!--  Nội dung nằm ở đây -->
-                        <H1>TESTTTTT</H1>
+                        <H1>CODE MỚI NÈEEE</H1>
                     </div>
                 </div>
             </div>
